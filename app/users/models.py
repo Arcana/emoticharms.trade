b1fa02c0b179eb7b9daed8d71daf6c3b995d55f2
Ben <ben@smash.mn>
2015-07-18 01:36:17 +0100

    Make start, with user log in

---
+++ app/users/models.py
@@ -0,0 +1,103 @@
+from app import db, steam
+from steam.api import HTTPError, HTTPTimeoutError
+from flask.ext.login import AnonymousUserMixin
+import datetime
+
+
+class AnonymousUser(AnonymousUserMixin):
+    user_class = 0
+
+    @staticmethod
+    def update_steam():
+        return False
+
+    @staticmethod
+    def is_admin():
+        return False
+
+    @staticmethod
+    def is_uploader():
+        return False
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    account_id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    name = db.Column(db.String(256, collation="utf8_swedish_ci"), default=account_id)
+    profile_url = db.Column(db.String(128))
+    avatar_small = db.Column(db.String(128))
+    avatar_medium = db.Column(db.String(128))
+    avatar_large = db.Column(db.String(128))
+    joined = db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
+    last_seen = db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
+    next_steam_check = db.Column(db.DateTime, default=datetime.datetime.utcnow() + datetime.timedelta(hours=4))
+    user_class = db.Column(db.Integer, default=0)
+    upload_credits = db.Column(db.Integer, default=0)
+    signed_in = db.Column(db.Boolean, default=True)
+    enabled = db.Column(db.Boolean, default=True)
+
+    __mapper_args__ = {
+        "order_by": [db.asc(joined)]
+    }
+
+    def __init__(self, account_id=None, signed_in=None, last_seen=None):
+        self.account_id = account_id
+        self.signed_in = signed_in
+        self.last_seen = last_seen
+        self.fetch_steam_info()
+
+    def __repr__(self):
+        return self.name
+
+    def get_id(self):
+        return unicode(self.account_id)
+
+    def is_active(self):
+        return self.enabled
+
+    @staticmethod
+    def is_anonymous():
+        return False
+
+    @staticmethod
+    def is_authenticated():
+        return True
+
+    def is_admin(self):
+        return True if self.user_class > 1 else False
+
+    def is_uploader(self):
+        return True if self.upload_credits > 0 or self.upload_credits == -1 or self.is_admin() else False
+
+    def update_last_seen(self):
+        now = datetime.datetime.utcnow()
+        if not self.next_steam_check:
+            self.next_steam_check = datetime.datetime.utcnow()
+        self.last_seen = now
+        if self.next_steam_check < now:
+            self.fetch_steam_info()
+        db.session.add(self)
+        db.session.commit()
+
+    def fetch_steam_info(self):
+        steam_info = steam.user.profile(self.steam_id)
+        self.update_steam_info(steam_info)
+
+    def update_steam_info(self, steam_info):
+        try:
+            self.name = steam_info.persona
+            self.profile_url = steam_info.profile_url
+            self.avatar_small = steam_info.avatar_small
+            self.avatar_medium = steam_info.avatar_medium
+            self.avatar_large = steam_info.avatar_large
+            self.next_steam_check = datetime.datetime.utcnow() + datetime.timedelta(hours=4)
+        except (HTTPError, HTTPTimeoutError):
+            self.next_steam_check = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+
+    @property
+    def steam_id(self):
+        return self.account_id + 76561197960265728
+
+    @property
+    def perma_profile_url(self):
+        return "http://steamcommunity.com/profiles/{}".format(self.steam_id)
---
+++ app/users/views.py
@@ -0,0 +1,78 @@
+from flask import Blueprint, flash, redirect, url_for
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import oid, db, login_manager
+from models import User, AnonymousUser
+from sqlalchemy.exc import IntegrityError
+
+
+users = Blueprint("users", __name__, url_prefix="/users")
+
+login_manager.anonymous_user = AnonymousUser
+
+
+# User authentication
+@login_manager.user_loader
+def load_user(user_id):
+    _user = User.query.get(user_id)
+    if _user:
+        _user.update_last_seen()
+    if _user and _user.enabled is False:
+        logout_user()
+        flash("You have been banned from using this website.", "danger")
+        redirect(url_for("index"))
+    return _user
+
+
+@users.route('/login/')
+@oid.loginhandler
+def login():
+    if current_user.is_authenticated():
+        return redirect(oid.get_next_url())
+    return oid.try_login('http://steamcommunity.com/openid')
+
+
+@oid.after_login
+def create_or_login(resp):
+    steam_id = long(resp.identity_url.replace("http://steamcommunity.com/openid/id/", ""))
+    account_id = int(steam_id & 0xFFFFFFFF)
+    _user = User.query.get(account_id)
+    new_user = False
+
+    if not _user:
+        _user = User(account_id)
+        new_user = True
+        db.session.add(_user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            _user = User.query.get(account_id)
+
+    if not _user.signed_in:
+        _user.signed_in = True
+        db.session.add(_user)
+        db.session.commit()
+
+    if not _user.is_active():
+        flash(u"Cannot log you in, {}. You are banned.".format(_user.name), "danger")
+        return redirect(oid.get_next_url())
+
+    login_attempt = login_user(_user, remember=True)
+    if login_attempt is True and new_user and _user.profile_url:
+        flash(u"Welcome to mods.tf, {}!".format(_user.name), "success")
+    elif login_attempt is True and new_user and not _user.profile_url:
+        flash(u"Welcome to mods.tf! Unfortunately were unable to fetch your Steam user data."
+              u"We will try again soon. For now you will be represented by your numerical ID, {}.".format(_user.name), "success")
+    elif login_attempt is True and not new_user:
+        flash(u"Welcome back, {}.".format(_user.name), "success")
+    else:
+        flash(u"Error logging you in as {}, please try again later.".format(_user.name), "danger")
+    return redirect(oid.get_next_url())
+
+
+@users.route('/logout/')
+@login_required
+def logout():
+    logout_user()
+    flash(u"You are now logged out.")
+    return redirect(oid.get_next_url())
